99a9f74570650d188a2faeac5a7392e09cad623c
Anna Panas <annapanas0906@gmail.com>
2020-02-03 22:54:16 -0500

    created a server, made routes to index.html and notes.html pages

---
+++ Develop/Store.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+// creating a server
+const app = express();
+const PORT = 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+
+
+
+// making a route to notes.html file
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
+})
+
+// making a route to index.html file
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+
+// Starts our server
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
