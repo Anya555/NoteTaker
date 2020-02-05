3016516dd23bc536add21cec4c1a905e41645b48
Anna Panas <annapanas0906@gmail.com>
2020-02-04 20:33:24 -0500

    css and js files load correctly after adding express.static(). added route to db.json file. made class Store

--- Develop/Store.js
+++ Develop/Store.js
@@ -9,26 +9,38 @@ const PORT = 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
+
+// making class Store to handle reading and writing data 
+class Store {
+    constructor(title, text){
+    this.title = title,
+    this.text = text
+    }
+}
 
 // Routes
 
-
-
 // making a route to notes.html file
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/notes.html"))
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
 })
+
+// making a route to db.json file
+app.get("/api/notes", function(req, res){
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+
+    // let newNote = new Store();
+});
+
+
 
 // making a route to index.html file
 app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-//reads the db.json file and returns all saved notes as JSON
-app.get("api/notes", function(req, res){
-    res.sendFile(path.join(__dirname, "./db/db.json"));
-    console.log(res);
-})
+
 
 
 // Starts our server
