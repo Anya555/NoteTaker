eac2d5dec0f8d48c0040f0d47cb2fbc34edf51ae
Anna Panas <annapanas0906@gmail.com>
2020-02-05 20:09:38 -0500

    working on deleting notes

--- Develop/db/Store.js
+++ Develop/db/Store.js
@@ -1,7 +1,8 @@
 // making class Store to handle reading and writing data
 class Store {
   constructor(title, text) {
-    this.id = Math.floor(Math.random() * (100000 - 1 + 1)) + 1;
+  //Math.random method will generate random id for each note,so I can view and delete each of them individually   
+    this.id = Math.floor(Math.random() * (100000 - 1 + 1)) + 1; 
     this.title = title;
     this.text = text;
   }
--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -3,19 +3,36 @@ const Store = require("../db/Store");
 const path = require("path");
 const db = require("../db/db.json");
 
-let savedNotes = [];
-
 module.exports = function(app) {
-  // Should read the db.json file and return all saved notes as JSON
+  // reads the db.json file and returns all saved notes as JSON
   app.get("/api/notes", function(req, res) {
     res.json(db);
   });
 
-  app.post("/api/notes", function(req, res) {
-    // res.sendFile(path.join(__dirname, "../db/db.json"));
-
+  app.delete("/api/notes/:id", function(req, res) {
     let storeObj = db;
 
+    // making new Store object to access id parameter of each note
+    let noteId = new Store (req.body.id);
+
+
+   let newStoreObj = storeObj.filter(filteredNote => {
+      return filteredNote.id != noteId;
+    });
+
+
+    fs.writeFile("./db/db.json", JSON.stringify(newStoreObj), function(err){
+      res.json(newStoreObj);  
+  });
+});
+
+  //Should receive a new note to save on the request body, add it to the db.json file,
+  // and then return the new note to the client.
+  app.post("/api/notes", function(req, res) {
+  
+    let storeObj = db;
+
+  // making new note as an object so I can add it to an array in db.json file and display in browser
     let nextNote = new Store(req.body.title, req.body.text);
     storeObj.push(nextNote);
 
@@ -24,4 +41,4 @@ module.exports = function(app) {
     });
   });
 
-};
+}
